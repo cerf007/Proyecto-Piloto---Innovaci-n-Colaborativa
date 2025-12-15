0a9d3544e2eb93e93b118f40169213d79880038b
cerf007 <cerf02062007@gmail.com>
2025-12-15 12:41:20 -0600

    Ejemplo de version 2

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,1 +1,20 @@
-print("Hello world")
+tareas = []
+
+def agregar_tarea(nombre, responsable):
+    tarea = {
+        "nombre": nombre,
+        "responsable": responsable,
+        "estado": "Pendiente"
+    }
+    tareas.append(tarea)
+    print("Tarea agregada correctamente")
+
+def mostrar_tareas():
+    print("\nLista de tareas:")
+    for i, tarea in enumerate(tareas, start=1):
+        print(f"{i}. {tarea['nombre']} - {tarea['responsable']} - {tarea['estado']}")
+
+agregar_tarea("Diseñar material educativo", "Diseñador instruccional")
+agregar_tarea("Revisar contenido final", "Revisor")
+
+mostrar_tareas()
